a44b0dfb40b5c95057fbb541159b381ca57f07d2
alicefirminger <alicefirminger@gmail.com>
2023-05-26 17:29:56 +0100

    changes js to scrolling works better and finished contact section

--- main.js
+++ main.js
@@ -1,35 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const aboutLink = document.querySelector('a[href="#about"]');
-    const projectsLink = document.querySelector('a[href="#projects"]');
-    const skillsLink = document.querySelector('a[href="#skills"]');
-    const contactLink = document.querySelector('a[href="#contact"]');
+	const aboutLink = document.querySelector('a[href="#about"]');
+	const projectsLink = document.querySelector('a[href="#projects"]');
+	const skillsLink = document.querySelector('a[href="#skills"]');
+	const contactLink = document.querySelector('a[href="#contact"]');
 
-    const scrollToSection = function (event, sectionId) {
-      event.preventDefault();
-      const section = document.getElementById(sectionId);
-      const sectionOffsetTop = section.offsetTop;
-      const windowHeight = window.innerHeight;
-      const scrollToPosition = sectionOffsetTop - windowHeight / 2;
+	const scrollToSection = function (event, sectionId) {
+		event.preventDefault();
+		const section = document.getElementById(sectionId);
+		const sectionOffsetTop = section.offsetTop;
+		const windowHeight = window.innerHeight;
+		const scrollToPosition = sectionOffsetTop - windowHeight / 8;
 
-      window.scrollTo({
-        top: scrollToPosition,
-        behavior: "smooth",
-      });
-    };
+		window.scrollTo({
+			top: scrollToPosition,
+			behavior: "smooth",
+		});
+	};
 
-    aboutLink.addEventListener("click", function (event) {
-      scrollToSection(event, "about");
-    });
+	aboutLink.addEventListener("click", function (event) {
+		scrollToSection(event, "about");
+	});
 
-    projectsLink.addEventListener("click", function (event) {
-      scrollToSection(event, "projects");
-    });
+	projectsLink.addEventListener("click", function (event) {
+		scrollToSection(event, "projects");
+	});
 
-    skillsLink.addEventListener("click", function (event) {
-      scrollToSection(event, "skills");
-    });
+	skillsLink.addEventListener("click", function (event) {
+		scrollToSection(event, "skills");
+	});
 
-    contactLink.addEventListener("click", function (event) {
-      scrollToSection(event, "contact");
-    });
-  });
+	contactLink.addEventListener("click", function (event) {
+		scrollToSection(event, "contact");
+	});
+});
