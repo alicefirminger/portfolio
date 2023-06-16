905969c2189ea5da73e50d6cf44bebe87e34463c
alicefirminger <alicefirminger@gmail.com>
2023-06-16 18:09:29 +0100

    added content to one html page

--- about.js
+++ about.js
@@ -1,15 +1,15 @@
 let i = 0;
-const txt = 'project: random recipe generator'; /* The text */
+const txt = "project: random recipe generator"; /* The text */
 const speed = 70; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("title").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
+	if (i < txt.length) {
+		document.getElementById("title").innerHTML += txt.charAt(i);
+		i++;
+		setTimeout(typeWriter, speed);
+	}
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    typeWriter();
+document.addEventListener("DOMContentLoaded", function () {
+	typeWriter();
 });
